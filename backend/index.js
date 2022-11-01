bb1b1590d88b207c91096e502c20ce2bdeae7c82
Thikhina Bakmeedeniya <thikhinab@gmail.com>
2022-11-02 02:20:09 +0800

    Remove mongodb memory server

--- backend/index.js
+++ backend/index.js
@@ -2,46 +2,24 @@ import express from "express";
 import mongoose from "mongoose";
 import parser from "body-parser";
 import dotenv from "dotenv";
-import { MongoMemoryServer } from "mongodb-memory-server";
 
 import bookRouter from "./routes/book-router.js";
 dotenv.config();
-console.log(process.env.VIMUTH);
 const env = process.env.NODE_ENV || "development";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 // Set up database
-if (env === "production") {
-  mongoose.connect(
-    process.env.MONGODB_URI_PROD,
-    () => console.log("Connected to Database"),
-    (err) => console.log(err)
-  );
-} else {
-  (async () => {
-    const mongoServer = await MongoMemoryServer.create();
-    const mongoUri = mongoServer.getUri();
+const mongoDbUri =
+  env === "production"
+    ? process.env.MONGODB_URI_PROD
+    : process.env.MONGODB_URI_DEV;
 
-    await mongoose.connect(
-      mongoUri,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      },
-      () => console.log("Connected to Database"),
-      (err) => console.log(err)
-    );
-  })();
-}
-
-// Debugging
-console.log(
-  "variables:",
-  env,
-  process.env.MONGODB_URI_PROD,
-  process.env.VIMUTH
+mongoose.connect(
+  mongoDbUri,
+  () => console.log("Connected to Database"),
+  (err) => console.log(err)
 );
 
 app.use("/api/books", parser.json(), bookRouter);
